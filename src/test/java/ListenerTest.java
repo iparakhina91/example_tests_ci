28d346080410d566bf8beb5f0fa543ba4209e997
Irina <iparakhina91@gmail.com>
2023-04-23 16:15:01 +0300

    first commit

---
+++ src/test/java/ListenerTest.java
@@ -0,0 +1,37 @@
+package com.gmail.iparakhina91;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class ListenerTest {
+
+    @Test
+    @Feature("Issue в репозитории")
+    @Story("Поиск Issue в репозитории")
+    @Owner("korovinaiyu")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(value = "Testing", url = "https://github.com")
+    @DisplayName("Поиск Issue по номеру")
+    public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#80")).should(Condition.exist);
+    }
+}
